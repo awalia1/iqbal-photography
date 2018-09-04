0ed219b0183d24ac1762b504f8225d84b103fea7
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-09-04 15:14:57 -0400

    added function for contact form

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ window.addEventListener('scroll', function(){
   		} else {
             nav.classList.add('hidden');
             heroText.classList.remove('hidden');
-          }
+        }
 	})
 
 function menuReveal(x) {
@@ -21,3 +21,14 @@ function menuReveal(x) {
 
 var about = document.getElementById("about-text");
 about.innerHTML = "Iqbal Photography is led by Asma Iqbal, who is armed with years of experience as a professional photographer. Asma's dedication to her craft is the reason why her ability to capture the moment and her subjects is unmatched across the industry. At Iqbal Photography, we specialize in all types of events from wedding, proposals to profiles and corporate events.";
+
+function contactFunction() {
+    var name =  document.forms["contactForm"]["name"];
+    var email = document.forms["contactForm"]["mail"];
+    var comment =  document.forms["contactForm"]["message"];
+
+    alert('Your message has been sent. Thank you!');
+    name.value = "";
+    email.value = "";
+    comment.value = "";
+}
