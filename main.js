86d90467f885e43ff9a6d0807e7c1bf9e158f459
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-09-03 21:49:00 -0400

    finished basic nav for mobile

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ console.log("i'm here");
 
 var nav = document.getElementById('nav');
 
-// window.addEventListener('scroll', function(){
-//     scrollTarget = 0;
-//     	if (window.scrollY > scrollTarget) {
-//     		nav.classList.remove('hidden');
-//   		}
-// 	})
+window.addEventListener('scroll', function(){
+    scrollTarget = 5;
+    	if (window.scrollY > scrollTarget) {
+    		nav.classList.remove('hidden');
+  		}
+	})
 
 function menuReveal(x) {
     x.classList.toggle("change");
+    var menuItems = document.getElementById('menu-items');
+    menuItems.classList.toggle("hidden");
 }
