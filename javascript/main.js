74ed50ec704d910cfe4751b0507fc769dd1fd943
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-09-07 19:19:42 -0400

    finished about section/ added slideshow

--- javascript/main.js
+++ javascript/main.js
@@ -11,3 +11,34 @@ function contactFunction() {
     email.value = "";
     comment.value = "";
 }
+
+imgArray = [
+    // 'images/slide-kid.JPG',
+    'images/slide-kid(2).JPG',
+    'images/slide-kid(3).JPG'
+];
+
+var gallery = document.getElementById("gallery")
+
+imgArray.forEach(function(image){
+    var img = document.createElement('img');
+    img.className = "mySlides";
+    img.src = image;
+    document.getElementById('slideshowImg').appendChild(img);
+});
+
+var myIndex = 0;
+carousel();
+
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
