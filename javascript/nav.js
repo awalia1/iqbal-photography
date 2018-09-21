edf0c549bf6d6f566e571fad40e9d017ddf730ed
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-09-21 14:23:22 -0400

    Revert "added more transition to navbar"
    This reverts commit d4b3315b0b5c57c889508450cf7e6ac925cd7d29.

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function(){
     var nav = document.getElementById('nav');
     var heroText = document.getElementById('hero-text');
-    scrollTarget = 20;
+    scrollTarget = 10;
     	if (window.scrollY > scrollTarget) {
             nav.classList.remove("invisible");
   		} else {
@@ -12,7 +12,7 @@ window.addEventListener('scroll', function(){
 var menuItems = document.getElementById('menu-items');
 
 function menuReveal() {
-    menuItems.classList.toggle("left");
+    menuItems.classList.toggle("hidden");
     // if (menuItems.style.display === "none") {
     //     menuItems.style.display = "block";
     // } else {
